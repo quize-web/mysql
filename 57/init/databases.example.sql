5d2d4559ddcd81d7a6f5e2fce1bb3a2b3d79711b
Илья Петухов <ilya@MacBook-Pro-2020.local>
2020-10-12 11:17:57 +0300

    first commit.

---
+++ 57/init/databases.example.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS `example`;
+GRANT ALL ON `example`.* TO 'docker'@'%';
+
+CREATE DATABASE IF NOT EXISTS `custom_example`;
+GRANT ALL ON `custom_example`.* TO 'docker'@'%';
